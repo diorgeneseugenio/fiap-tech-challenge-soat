4cebd61e2f4bbdfad19ef88be00e22bc128ad39d
EltonARodrigues <elton.rodrigues61@gmail.com>
2023-05-28 15:28:32 -0300

    refactor: config e index
    O config foi separado de diretório mantendo o do banco de dados no
    driven e o server no driver da api.
    
    Por enquanto o config do banco de dados está desabilitado.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
 
-import { DataBaseConfig } from "./config/db.config";
-import { Server } from "./config/server.config";
-import routerExample from "./routes/example";
+// import { DataBaseConfig } from "../../../config/db.config";
+import { Server } from "./adapter/driver/api/config/server.config";
+import { produtoRouter } from "./adapter/driver/api/routers/index";
 
 dotenv.config();
 
-const database = new DataBaseConfig({
-  host: "localhost",
-  userName: "root",
-  password: "admin",
-  port: 3306,
-});
+// const database = new DataBaseConfig({
+//     host: "localhost",
+//     userName: "root",
+//     password: "admin",
+//     port: 3306,
+// });
 
 const app: Express = express();
 
-const server = new Server({ appConfig: app, dataBaseConfig: database });
+const server = new Server({ appConfig: app, });
 
-server.addRouter("/api/example", routerExample);
+server.addRouter("/api/produto", produtoRouter);
 
 server.init();
+
+// console.log('hello world!2')
