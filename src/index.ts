e31b68a497201994d1513cd2b4c4156727ae9c76
EltonARodrigues <elton.rodrigues61@gmail.com>
2023-06-06 22:03:46 -0300

    fix: aplicado as sugestões de correção
    - Adiciona createAt, updateAt e deletedAt
    - Atualizado de UUIDV1 para UUIDV4
    - Utilizando um novo padrão para as ações
    - synchronizeModels import de um unico model
    - Correção em alguns typos
    - Novas tratativas no controller

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
 
-// import { DataBaseConfig } from "../../../config/db.config";
+import { DataBaseConfig } from "./adapter/driven/infra/config/db.config";
 import { Server } from "./adapter/driver/api/config/server.config";
 import { produtoRouter } from "./adapter/driver/api/routers/index";
+import Modelos from "./adapter/driven/infra/models/index";
 
 dotenv.config();
 
-// const database = new DataBaseConfig({
-//     host: "localhost",
-//     userName: "root",
-//     password: "admin",
-//     port: 3306,
-// });
+const database = new DataBaseConfig({
+    database: process.env.DB_NAME ?? "projeto",
+    host: process.env.DB_HOST ?? "localhost",
+    userName: process.env.DB_USERNAME ?? "root",
+    password: process.env.DB_PASSWORD ?? "testtest",
+    port: 3306,
+})
+
+database.authenticate();
+database.synchronizeModels(Modelos);
 
 const app: Express = express();
 
 const server = new Server({ appConfig: app, });
 
 server.addRouter("/api/produto", produtoRouter);
-server.addRouter("/api/produto", produtoRouter);
 
 server.init();
-
-// console.log('hello world!2')
