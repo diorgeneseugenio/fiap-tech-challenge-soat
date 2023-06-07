7df0c60c4850e8ca504fa33f4fe6bba4ab1cbff4
EltonARodrigues <elton.rodrigues61@gmail.com>
2023-06-06 22:03:46 -0300

    fix: aplicado as sugestões de correção
    - Adiciona createAt, updateAt e deletedAt
    - Atualizado de UUIDV1 para UUIDV4
    - Utilizando um novo padrão para as ações
    - synchronizeModels import de um unico model
    - Correção em alguns typos
    - Novas tratativas no controller

--- src/core/applications/ports/produtoRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Produto } from "core/domain/produto";
-
-export default interface ProdutoRepository {
-    criaProduto(produto: Produto): Promise<Produto>;
-    deletaProduto(idProduto: string): Promise<any>;
-    editaProduto(idProduto: string, produto: Produto): Promise<Produto|undefined>;
-    listaProdutos(filtro: any): Promise<Produto[]>;
-    retornaProduto(idProduto: string): Promise<any>;
-}
