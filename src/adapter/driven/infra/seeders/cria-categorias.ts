41ed07d34820ef91e123883fb33a9be20e67b6d1
EltonARodrigues <elton.rodrigues61@gmail.com>
2023-06-08 13:39:27 -0300

    feat: criado seed para tabela categorias

---
+++ src/adapter/driven/infra/seeders/cria-categorias.ts
@@ -0,0 +1,22 @@
+import { QueryInterface, Sequelize } from "sequelize";
+import { v4 as uuidv4 } from 'uuid';
+export default {
+  async up(queryInterface: QueryInterface, Sequelize: Sequelize) {
+    const countCategoriasRow: any = await queryInterface.sequelize.query(
+      'SELECT COUNT(*) AS count FROM Categorias'
+    );
+
+    if (countCategoriasRow[0][0].count === 0) {
+      await queryInterface.bulkInsert('Categorias', [
+        { id: uuidv4(), nome: 'Lanche', created_at: new Date(), updated_at: new Date() },
+        { id: uuidv4(), nome: 'Acompanhamento', created_at: new Date(), updated_at: new Date() },
+        { id: uuidv4(), nome: 'Bebida', created_at: new Date(), updated_at: new Date() },
+        { id: uuidv4(), nome: 'Sobremesa', created_at: new Date(), updated_at: new Date() },
+      ]);
+    }
+  },
+
+  async down(queryInterface: QueryInterface, Sequelize: Sequelize) {
+    // Nao implementado
+  }
+};
