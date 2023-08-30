6ab1c7da14cff60a1da997a5ed12880cfe2d92c6
Elton A Rodrigues <eltonrodrigues61@gmail.com>
2023-08-29 22:17:58 -0300

    fix: deixa entidade usuario publica

--- src/domain/entities/usuario.ts
+++ src/domain/entities/usuario.ts
@@ -3,44 +3,22 @@ import { v4 as uuidv4 } from "uuid";
 import UsuarioInput from "./types/UsuarioType";
 
 export default class Usuario {
-  private _id: string;
-  private _cpf: string | null;
-  private _email: string | null;
-  private _nome: string;
-  private _createdAt: Date;
-  private _deletedAt: Date | null;
-  private _updatedAt: Date | null;
+  public id: string;
+  public cpf: string | null;
+  public email: string | null;
+  public nome: string;
+  public createdAt: Date;
+  public deletedAt: Date | null;
+  public updatedAt: Date | null;
 
   constructor(usuarioInput: UsuarioInput) {
-    this._id = usuarioInput.id ?? uuidv4();
-    this._email = usuarioInput.email ?? null;
-    this._cpf = usuarioInput.cpf ?? null;
-    this._nome = !this._cpf && !this._email && usuarioInput.nome ? 'Anonimo' : usuarioInput.nome as string
-    this._createdAt = usuarioInput.createdAt ?? new Date();
-    this._deletedAt = usuarioInput.deletedAt ?? null;
-    this._updatedAt = usuarioInput.updatedAt ?? null;
+    this.id = usuarioInput.id ?? uuidv4();
+    this.email = usuarioInput.email ?? null;
+    this.cpf = usuarioInput.cpf ?? null;
+    this.nome = !this.cpf && !this.email && usuarioInput.nome ? 'Anonimo' : usuarioInput.nome as string
+    this.createdAt = usuarioInput.createdAt ?? new Date();
+    this.deletedAt = usuarioInput.deletedAt ?? null;
+    this.updatedAt = usuarioInput.updatedAt ?? null;
 
-  }
-
-  get id(): string | undefined {
-    return this._id;
-  }
-  get cpf(): string | null  {
-    return this._cpf;
-  }
-  get email(): string | null   {
-    return this._email;
-  }
-  get nome(): string  {
-    return this._nome;
-  }
-  get createdAt(): Date{
-    return this._createdAt;
-  }
-  get deletedAt(): Date | null{
-    return this._deletedAt;
-  }
-  get updatedAt(): Date | null {
-    return this._updatedAt;
   }
 }
